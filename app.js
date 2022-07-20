f889efa700c229ddd973f73ce522148e912caed0
Sudhanshu Pandey <sudhanshupandey920@gmail.com>
2022-07-20 20:42:39 +0530

    weather report v1

---
+++ app.js
@@ -0,0 +1,31 @@
+const express=require("express");
+const https=require("https");
+const bodyParser=require("body-parser");
+
+const app=express();
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",function(req,res){
+
+    res.sendFile(__dirname+"/index.html");
+})
+
+app.post("/",function(req,res){
+    const url="https://api.openweathermap.org/data/2.5/weather?q="+req.body.city+"&appid=8ef46f92d7d2fe27171fc1f57cd70658&units=metric";
+
+    https.get(url,function(response){
+        response.on("data",function(data){
+            const weatherData=JSON.parse(data);
+            res.write("<p>the condition in "+req.body.city+" is "+weatherData.weather[0].description+"</p>")
+            res.write("<h1>the temperature of "+req.body.city+" is "+weatherData.main.temp+" degree celcius</h1>")
+            const icon="http://openweathermap.org/img/wn/"+weatherData.weather[0].icon+"@2x.png"
+            res.write("<img src="+icon+">")
+            res.send();
+        })
+    })
+})
+
+
+app.listen(3000,function(){
+    console.log("server started running on port 3000");
+})
